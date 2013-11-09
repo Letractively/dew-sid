aa782d7350b97ac5e1bc795c98c22eb658e19ca9
vfloresupc <vfloresupc@48963df4-430d-f861-5695-dcca7708c147>
2013-11-09 02:02:21 +0000

    Este es el proyecto java

---
+++ Web/src/java/sid/negocio/GestionResidente.java
@@ -0,0 +1,70 @@
+package sid.negocio;
+
+import java.sql.SQLException;
+import java.util.Collection;
+import sid.persistencia.ResidenteDAO;
+import sid.persistencia.DAOExcepcion;
+import sid.modelo.Residente;
+
+public class GestionResidente { //aqui va la regla de negocio
+    
+    public Residente insertar(String nombre,String apellido,String dni,String fecha_nac,String email,String pwd)throws DAOExcepcion
+    {
+        ResidenteDAO dao = new ResidenteDAO();
+        Residente    vo  = new Residente();
+        //vo.setIdresidente(idresidente);
+        vo.setNombres(nombre);
+        vo.setApellidos(apellido);
+        vo.setDni(dni);
+        vo.setFech_nac(fecha_nac);
+        vo.setEmail(email);
+        vo.setPassword(pwd);
+        //dao.buscarporcorreo(email);
+        Collection<Residente> lista = dao.buscarporcorreo(email);//aqui ingreso el correo para buscarlo
+        if(lista.size()>0){
+            throw new DAOExcepcion("Este correo existe");   //Este es para mostrar un mensaje de exception
+        }else{
+            return dao.insertar(vo);
+        }
+        
+   }
+    
+   public Residente actualizar(int idresidente,String nombre,String apellido,String dni,String fecha_nac,String email,String pwd) throws DAOExcepcion{
+     ResidenteDAO re = new ResidenteDAO();
+     Residente    vo = new Residente();
+     vo.setIdresidente(idresidente);
+     vo.setNombres(nombre);
+     vo.setApellidos(apellido);
+     vo.setDni(dni);
+     vo.setFech_nac(fecha_nac);
+     vo.setEmail(email);
+     vo.setPassword(pwd);
+     return re.actualizar(vo);
+   }
+   
+   /*public Residente eliminar(int idresidente ) throws DAOExcepcion{ //Aqui le envio parametros
+       ResidenteDAO eli = new ResidenteDAO();
+       Residente    vo  = new Residente();
+       vo.setIdresidente(idresidente);      //Aqui asigno el valor (SET)
+       return eli.eliminar(vo);
+   }*/
+   
+   public void eliminar(int idResidente) throws DAOExcepcion {  //Esto no es una funcion, es un procedimiento y no retorna nada, en este caso le envio un parametro
+	ResidenteDAO eli = new ResidenteDAO();
+	eli.eliminar(idResidente);
+  }
+   
+  public Collection<Residente> buscarporcorreo(String correo) throws DAOExcepcion {
+	ResidenteDAO dao = new ResidenteDAO();
+	return dao.buscarporcorreo(correo);
+  }
+   
+   public Collection<Residente> listar() throws DAOExcepcion{
+      ResidenteDAO dao = new ResidenteDAO();
+      return dao.listar();
+  }
+    
+    
+    
+    
+}
---
+++ Web/test/sid/negocio/GestionResidenteTest.java
@@ -0,0 +1,81 @@
+package sid.negocio;
+
+import java.util.Collection;
+import org.junit.Assert;
+import sid.persistencia.DAOExcepcion;
+import sid.modelo.Residente;
+import sid.negocio.GestionResidente;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GestionResidenteTest {
+    
+    //@Test
+    public void insertarTest() throws DAOExcepcion{
+        GestionResidente negocio = new GestionResidente();
+        try{
+           negocio.insertar("Jesus", "Alvarez", "40924578", "1980-10-02", "jesus25@hotmail.com", "287-6700"); //1980-10-89
+            
+        }catch(DAOExcepcion e){
+           Assert.fail("Fallo la insercion: " + e.getMessage());
+        }
+    
+    }
+    
+   //@Test
+    public void actualizarTest() throws DAOExcepcion{
+        GestionResidente negocio = new GestionResidente();
+        try{
+            negocio.actualizar(4, "Juan", "perez", "4463172", "1982-02-15", "peplucho@terra.com.pe", "2871055");
+        }catch(DAOExcepcion e){
+            Assert.fail("Fallo la actualizacion: " + e.getMessage());
+        }
+    
+    }
+    
+    //@Test
+    public void eliminarTest() throws DAOExcepcion{
+        GestionResidente negocio = new GestionResidente();
+        try{
+            negocio.eliminar(3);
+        }catch(DAOExcepcion x){
+            Assert.fail("Error, no elimina: " + x.getMessage());
+        }
+    }
+    
+    //@Test
+    public void listarTest(){
+        GestionResidente negocio = new GestionResidente();
+        try{
+            Collection<Residente> listado = negocio.listar();
+            //System.out.println(listado.size());
+            for(Residente r:listado){
+                System.out.println(r.getNombres());
+                System.out.println(r.getApellidos());
+                /*System.out.println(r.getDni());
+                System.out.println(r.getEmail());*/
+            }
+            Assert.assertTrue(listado.size()>0);
+        }catch(DAOExcepcion e){
+            Assert.fail("Fallo el listado: " + e.getMessage());
+        }
+    }
+    
+   /* @Test
+    public void verificacorreoTest() throws DAOExcepcion{
+       GestionResidente negocio = new GestionResidente(); 
+       try{
+           Collection<Residente> listamail = negocio.buscarporcorreo("poemasmix@hotmail.com");
+           for(Residente r:listamail){
+            System.out.println(r.getNombres());
+           }
+          
+           
+       }catch(DAOExcepcion e){              
+           Assert.fail("No se encontro:" + e.getMessage());
+       }
+    }*/
+    
+
+}
